7166f689d8e549736340c3b97f99391efb7a40d2
efesavci <efesavci12@gmail.com>
2025-08-26 15:42:16 +0300

    Solved problem with negative arguments and put input reader to create custom matrices and shift by a wanted amount.

--- Main.java
+++ Main.java
@@ -1,15 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) {
-        int[][] arr = new int[][]{{1,2,3,4}, {5,6,7,8},{9,10,11,12}};
-        System.out.println(Arrays.deepToString(ShiftArray(arr, 2)));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
 
+        boolean readInt = false;
+        String line = "";
+        int val = 0;
+        boolean readBorders = false;
+        String lengthStr, widthStr;
+        int length = 0, width = 0;
+        System.out.println("Enter the length and width of your wanted matrice: ");
+        while(!readBorders) {
+            try{
+                System.out.print("Length: ");
+                lengthStr = br.readLine();
+                length = Integer.parseInt(lengthStr);
+                System.out.print("Width: ");
+                widthStr = br.readLine();
+                width = Integer.parseInt(widthStr);
+                readBorders = true;
+            }catch (NumberFormatException ne) {
+                System.out.println("Invalid value, please enter an integer value.");
+            }
+        }
+        System.out.print("Enter an amount to shift the matrix: ");
+        while(!readInt) {
+            try {
+                line = br.readLine();
+                val = Integer.parseInt(line);
+                readInt = true;
+            } catch (NumberFormatException ne) {
+                System.out.println("Invalid value, please enter an integer value.");
+            }
+        }
+        int idx = 1;
+        int[][] arr = new int[length][width];
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                arr[i][j] = idx++;
+             }
+        }
+        System.out.println("Shifting the matrice: "+Arrays.deepToString(arr)+" by "+val+".");
+        System.out.println("Resulting array: "+Arrays.deepToString(ShiftArray(arr, val)));
     } // Only requirement is, that the inner matrices should be the same size.
 
-    public static int[][] ShiftArray(int[][] arr,int ShiftAmount){
+    public static int[][] ShiftArray(int[][] arr, int shiftAmount){
         int length = arr.length * arr[0].length;
-        int realShiftAmt = ShiftAmount % (length);
+
+        int realShiftAmt = shiftAmount > 0 ? shiftAmount % length : (length - ((shiftAmount * -1) % length));
         int[] temp = new int[length];
         int count = realShiftAmt;
         for (int[] ints : arr) {
